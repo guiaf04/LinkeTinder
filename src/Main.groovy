4535384511539a71e3ec1732e5df7eb007f43e45
guiaf04 <guilhermearaujo@alu.ufc.br>
2024-08-17 17:05:04 -0300

    feat: add Main file with a terminal menu

---
+++ src/Main.groovy
@@ -0,0 +1,32 @@
+static void main(String[] args) {
+  LinkeTinder system = new LinkeTinder()
+
+  Scanner scanner = new Scanner(System.in)
+
+  String option = ""
+
+  String message = """
+  Welcome to Linketinder!
+  Type: 
+    0 for exit the program
+    1 to show the candidates of the system
+    2 to show company's of the system
+  """
+
+  while(option != "0"){
+    println(message)
+    option = scanner.next()
+
+    switch (option){
+      case "0" :
+        break
+      case "1" :
+        println system.listCandidates()
+        break
+      case "2" :
+        println system.listCompanys()
+        break
+      default : println("Incorrect option, try again")
+    }
+  }
+}
