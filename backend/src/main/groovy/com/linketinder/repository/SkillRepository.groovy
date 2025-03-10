1bbcefd82959458005bc480d740d666a8aedcc86
guiaf04 <guilhermearaujo@alu.ufc.br>
2025-03-09 21:54:54 -0300

    refac: return optional for better validation

--- backend/src/main/groovy/com/linketinder/repository/SkillRepository.groovy
+++ backend/src/main/groovy/com/linketinder/repository/SkillRepository.groovy
@@ -1,9 +1,9 @@
 package com.linketinder.repository
 
-import com.linketinder.model.Candidate
+
 import com.linketinder.model.Skill
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface SkillRepository extends JpaRepository <Skill, Long>{
-    Skill findByName(String name)
+    Optional<Skill> findByName(String name)
 }
