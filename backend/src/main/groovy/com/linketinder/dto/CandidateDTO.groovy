ff06a36cbbe15bcade1e005508ddc0eb65447e38
guiaf04 <guilhermearaujo@alu.ufc.br>
2025-03-12 14:25:25 -0300

    feat: extend CandidateDTO to include skills and override equals/hashCode methods

--- backend/src/main/groovy/com/linketinder/dto/CandidateDTO.groovy
+++ backend/src/main/groovy/com/linketinder/dto/CandidateDTO.groovy
@@ -1,8 +1,10 @@
 package com.linketinder.dto
 
+import com.linketinder.model.Skill
+
 import java.time.LocalDate
 
-class CandidateDTO implements Serializable{
+class CandidateDTO extends UnmatchedCandidateDTO{
 
     Long id
     String firstName
@@ -12,18 +14,39 @@ class CandidateDTO implements Serializable{
     String country
     String cep
     String personalDescription
+    List<Skill> skills
 
-    @Override
-    String toString() {
-        return "Candidato{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", dataNascimento='" + birthday + '\'' +
-                ", cpf='" + cpf + '\'' +
-                ", country='" + country + '\'' +
-                ", cep='" + cep + '\'' +
-                ", descricaoPessoal='" + personalDescription + '\'' +
-                '}'
+    boolean equals(o) {
+        if (this.is(o)) return true
+        if (o == null || getClass() != o.class) return false
+
+        CandidateDTO that = (CandidateDTO) o
+
+        if (birthday != that.birthday) return false
+        if (cep != that.cep) return false
+        if (country != that.country) return false
+        if (cpf != that.cpf) return false
+        if (firstName != that.firstName) return false
+        if (id != that.id) return false
+        if (lastName != that.lastName) return false
+        if (personalDescription != that.personalDescription) return false
+        if (skills != that.skills) return false
+
+        return true
+    }
+
+    int hashCode() {
+        int result
+        result = (id != null ? id.hashCode() : 0)
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0)
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0)
+        result = 31 * result + (birthday != null ? birthday.hashCode() : 0)
+        result = 31 * result + (cpf != null ? cpf.hashCode() : 0)
+        result = 31 * result + (country != null ? country.hashCode() : 0)
+        result = 31 * result + (cep != null ? cep.hashCode() : 0)
+        result = 31 * result + (personalDescription != null ? personalDescription.hashCode() : 0)
+        result = 31 * result + (skills != null ? skills.hashCode() : 0)
+        return result
     }
 }
 
