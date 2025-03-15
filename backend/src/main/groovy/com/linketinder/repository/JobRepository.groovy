bfe65545f1d726ad432cc7ea07a6582ad488839d
guiaf04 <guilhermearaujo@alu.ufc.br>
2025-03-15 15:35:35 -0300

    feat: add method to check existence of Job by name, description, employee ID, and local

--- backend/src/main/groovy/com/linketinder/repository/JobRepository.groovy
+++ backend/src/main/groovy/com/linketinder/repository/JobRepository.groovy
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface JobRepository extends JpaRepository <Job, Long>{
     List<Job> findAllByIdEmployee(Long employeeId)
+    boolean existsByNameAndDescriptionAndIdEmployeeAndLocal(String name, String description, Long idEmployee, String local)
 }
