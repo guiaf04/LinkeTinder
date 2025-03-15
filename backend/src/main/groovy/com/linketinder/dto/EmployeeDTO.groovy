f23e04790d7872c70f669dab45c272498f91385f
guiaf04 <guilhermearaujo@alu.ufc.br>
2025-03-14 23:56:12 -0300

    feat: implement equals and hashCode methods in EmployeeDTO for proper object comparison

--- backend/src/main/groovy/com/linketinder/dto/EmployeeDTO.groovy
+++ backend/src/main/groovy/com/linketinder/dto/EmployeeDTO.groovy
@@ -11,4 +11,31 @@ class EmployeeDTO {
     String country
     String cep
     String description
+
+    boolean equals(o) {
+        if (this.is(o)) return true
+        if (o == null || getClass() != o.class) return false
+
+        EmployeeDTO that = (EmployeeDTO) o
+
+        if (cep != that.cep) return false
+        if (cnpj != that.cnpj) return false
+        if (country != that.country) return false
+        if (description != that.description) return false
+        if (id != that.id) return false
+        if (name != that.name) return false
+
+        return true
+    }
+
+    int hashCode() {
+        int result
+        result = (id != null ? id.hashCode() : 0)
+        result = 31 * result + (name != null ? name.hashCode() : 0)
+        result = 31 * result + (cnpj != null ? cnpj.hashCode() : 0)
+        result = 31 * result + (country != null ? country.hashCode() : 0)
+        result = 31 * result + (cep != null ? cep.hashCode() : 0)
+        result = 31 * result + (description != null ? description.hashCode() : 0)
+        return result
+    }
 }
