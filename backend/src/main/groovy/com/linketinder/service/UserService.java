3d1e28535eb772fb7ffb57a3858c18aa1ec80df6
guiaf04 <guilhermearaujo@alu.ufc.br>
2025-03-16 13:09:25 -0300

    feat: add UserRepository and UserService for user management and authentication

---
+++ backend/src/main/groovy/com/linketinder/service/UserService.java
@@ -0,0 +1,25 @@
+package com.linketinder.service;
+
+import com.linketinder.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+  @Autowired
+  UserRepository userRepository;
+
+  public UserService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found"));
+  }
+}
